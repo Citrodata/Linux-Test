cad955885d730f0bb5f2a776fb87cd94a5a14709
M K <mxk@mxk>
2018-06-07 08:55:15 +0200

    Added Maven to the Project

---
+++ maven/src/main/java/de/max/test/App.java
@@ -0,0 +1,12 @@
+package de.max.test;
+
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class App 
+{
+    public static void main( String[] args ) throws UnsupportedAudioFileException, IOException
+    {
+    }
+}
---
+++ maven/src/test/java/de/max/test/AppTest.java
@@ -0,0 +1,16 @@
+package de.max.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+   @Test
+   public void test()
+   {
+	   assertTrue(true);
+	   assertEquals(0.01F, 0.02F, 0.03F);
+   }
+}
